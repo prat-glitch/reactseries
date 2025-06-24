cee97e0b21e2ab1bd5bc10df5a063623f9058c3b
Pratyush <pratyush321hq@gmail.com>
2025-06-24 08:24:20 +0530

    Added project to existing GitHub repo

--- file.js
+++ file.js
@@ -1,9 +1,31 @@
 
-//
-//
-let mydate= new Date()
-//
-let mynewdate= new Date("2025-01-8");
-//console.log(mynewdate.toLocaleString()); 
-console.log(mydate.toDateString());
-console.log(mydate.getDay()+1);
+const books=[
+    {
+        title:"book one " , genre:'history', publish:'2025'
+    },
+    {
+        titile:'book two', genre:"science", publish:'2023'
+    }
+]
+let userbooks= books.filter((bk)=> bk.publish==='2025')
+ userbooks=books.filter((bk) =>bk.publish>'2020' && bk.genre==='history')
+console.log(userbooks);
+const nums=[1,2,3,4]
+const ans=nums.reduce(function(acc, curval)
+{
+    console.log(`acc:${acc} and curval:${curval}`)
+    return acc+curval
+}, 0)
+console.log(ans);
+const shopping =[
+    {
+        items:'mobile dev',
+        price:999
+    },
+    {
+        items:'web dev',
+        price:1999
+    }
+]
+const pricetopay=shopping.reduce((acc , item) =>acc+item.price, 0)
+console.log(pricetopay);
